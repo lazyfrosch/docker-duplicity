e28fd5dec62eca8d2be419ff80a56a38f6c41329
Markus Frosch <markus@lazyfrosch.de>
2019-11-05 17:03:09 +0100

    Update test script

--- test.sh
+++ test.sh
@@ -5,9 +5,14 @@ set -e
 IMAGE=lazyfrosch/duplicity
 VOLUME=duplicity_test
 HOSTNAME=duplicity-test
+TARGET=file:///backup
 
 if [ -n "$1" ]; then
   IMAGE="$1"
+fi
+
+if [ -n "$2" ]; then
+  TARGET="$2"
 fi
 
 duplicity() {
@@ -15,7 +20,7 @@ duplicity() {
     -h "${HOSTNAME}" \
     -v "${VOLUME}:/backup" \
     -e "PASSPHRASE=${PASSPHRASE}" \
-    lazyfrosch/duplicity \
+    "${IMAGE}" \
     "$@"
 }
 
@@ -26,10 +31,10 @@ export PASSPHRASE="testtesttest"
 docker volume rm "$VOLUME" >/dev/null 2>/dev/null || true
 docker volume create "$VOLUME" >/dev/null
 
-duplicity full /usr file:///backup
-duplicity incr /usr file:///backup
-duplicity collection-status file:///backup
-duplicity verify file:///backup /usr
+duplicity full /usr "${TARGET}"
+duplicity incr /usr "${TARGET}"
+duplicity collection-status "${TARGET}"
+duplicity verify "${TARGET}" /usr
 
 docker volume rm "$VOLUME" >/dev/null
 
